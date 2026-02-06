84f6e08d9059bbfd935e4f3b5023cb4992bea532
Narendra Kolukula <kolukula.narendra10@gmail.com>
2026-02-06 17:44:31 +0530

    Minor changes to deploy

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
 // For production (Render backend)
-export const API_URL: string = "https://money-manager-backend.onrender.com";
-// Uncomment below for local development
+export const API_URL: string = "https://money-manager-backend-2aej.onrender.com";// Uncomment below for local development
 // export const API_URL = "http://localhost:8080";
